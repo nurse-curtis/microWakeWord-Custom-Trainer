9c4a86559a8fd138fe46356ab58fe5f09bc59483
MasterPhooey <106418429+MasterPhooey@users.noreply.github.com>
2025-01-04 21:16:43 -0600

    add advanced training

--- startup.sh
+++ startup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-# Check if the notebook exists in /data
+# Check if basic training notebook exists in /data
 if [ ! -f /data/basic_training_notebook.ipynb ]; then
-    echo "Notebook not found in /data. Copying the default notebook..."
+    echo "Basic training notebook not found in /data. Copying the default notebook..."
     cp /root/basic_training_notebook.ipynb /data/basic_training_notebook.ipynb
 else
-    echo "Notebook already exists in /data. Skipping copy."
+    echo "Basic training notebook already exists in /data. Skipping copy."
+fi
+
+# Check if advanced training notebook exists in /data
+if [ ! -f /data/advanced_training_notebook.ipynb ]; then
+    echo "Advanced training notebook not found in /data. Copying the default notebook..."
+    cp /root/advanced_training_notebook.ipynb /data/advanced_training_notebook.ipynb
+else
+    echo "Advanced training notebook already exists in /data. Skipping copy."
 fi
 
 exec "$@"
